fc30b4353fcabf552a3ee5317888d520b0e137f3
JohannLiebert1130 <672016243@qq.com>
2018-06-30 19:44:56 +0800

    add another socket for listening to the receiver

--- src/blindbox.py
+++ src/blindbox.py
@@ -2,22 +2,29 @@ import socket
 import subprocess
 from src.constants import OBLIVC_AES_PATH
 
-ADDRESS = ('127.0.0.1', 6060)
+ADDRESS = ('127.0.0.1', 6666)
+ADDRESS2 = ('127.0.0.1', 8785)
 
 rules = ["attack", "violence", "fuck", "shit", "damn"]
 
+
 class BlindBox:
     def __init__(self):
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._sock.bind(ADDRESS)
-        self._sock.listen(10)
+        self._sock_for_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock_for_s.bind(ADDRESS)
+        self._sock_for_s.listen(10)
+
+        self._sock_for_r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock_for_r.bind(ADDRESS2)
+        self._sock_for_r.listen(10)
 
     def detect(self):
         while True:
             # Wait for a connection
-            connection, address = self._sock.accept()
+            connection, address = self._sock_for_s.accept()
+            connection2, address2 = self._sock_for_r.accept()
 
-            self.rule_preparation(connection)
+            self.rule_preparation(connection, connection2)
 
             encrypted_data = connection.recv(20480)
             print(encrypted_data)
@@ -46,15 +53,22 @@ class BlindBox:
         #         pass
         pass
 
-    def rule_preparation(self, connection):
+    def rule_preparation(self, connection,  connection2):
         key_numbers = len(rules)
+        #
         connection.sendall(str(key_numbers).encode())
-        for rule in rules:
-            output = subprocess.getoutput(OBLIVC_AES_PATH + "/a.out 1235 localhost " + rule)
-            while output == "TCP connect failed":
-                output = subprocess.getoutput(OBLIVC_AES_PATH + "/a.out 1235 localhost " + rule)
+        connection2.sendall(str(key_numbers).encode())
 
-            print(output)
+        for rule in rules:
+            output1 = subprocess.getoutput(OBLIVC_AES_PATH + "/a.out 1235 localhost " + rule)
+            output2 = subprocess.getoutput(OBLIVC_AES_PATH + "/a.out 5321 localhost " + rule)
+            while output1 == "TCP connect failed":
+                output1 = subprocess.getoutput(OBLIVC_AES_PATH + "/a.out 1235 localhost " + rule)
+            while output2 == "TCP connect failed":
+                output2 = subprocess.getoutput(OBLIVC_AES_PATH + "/a.out 1235 localhost " + rule)
+            if output1[108:] != output2[108:]:
+                print('You are in danger!')
+                exit(1)
 
 
 if __name__ == '__main__':
